e7a07989e6751526e8d52064891c2eb887ff9601
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-24 02:18:40 +0300

    Sem_3_Task_18

---
+++ Seminars/Sem_3_Task_18/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 18: Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+int getQuarterFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation}: ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 1 || result > 4)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число от 1 до 4. {userInformation}: ");
+    }
+    return result;
+}
+string getCoordinateRangeFromQuarter(int quarterNumber)
+{
+    string result = "";
+    if (quarterNumber == 1)
+    {
+        result = ($"Четверть номер {quarterNumber} находится в координатах X > 0, Y > 0!");
+    }
+    else if (quarterNumber == 2)
+    {
+        result = ($"Четверть номер {quarterNumber} находится в координатах X > 0, Y < 0!");
+    }
+    else if (quarterNumber == 3)
+    {
+        result = ($"Четверть номер {quarterNumber} находится в координатах X < 0, Y < 0!");
+    }
+    else if (quarterNumber == 4)
+    {
+        result = ($"Четверть номер {quarterNumber} находится в координатах X < 0, Y > 0!");
+    }
+    else result = "Ошибка ввода!";
+    return result;
+}
+
+int quarterNumber = getQuarterFromUser("Введите номер четверти - целое число от 1 до 4");
+Console.WriteLine(getCoordinateRangeFromQuarter(quarterNumber));
