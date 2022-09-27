cd4538a26fdb2739291501879588c733d37c9ecc
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-28 01:35:54 +0300

    Number of digits

---
+++ Seminars/Sem_4_Task_26/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 
+Задача 26: Напишите программу, которая принимает на вход число 
+и выдаёт количество цифр в числе. 
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (userNumber > 0)
+{
+    userNumber = userNumber / 10;
+    count++;
+}
+Console.WriteLine($"Число состоит из {count} цифр!");
