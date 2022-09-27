09b48e9deffce2596930e5f7e180a35a87b0036c
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-28 01:09:47 +0300

    Sem_4_Task_24

--- Seminars/Sem_4_Task_24/Program.cs
+++ Seminars/Sem_4_Task_24/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 24: Напишите программу, которая принимает на вход число (А)
+ и выдаёт сумму чисел от 1 до А. 
+ 7 -> 28 
+ 4 -> 10 
+ 8 -> 36 
+ */
+
+void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();
+}
+int getSumOfRange(int startPoint, int endPoint)
+{
+    int result = 0;
+    if (startPoint > endPoint)
+    {
+        printInConsoleWithColor($"Верхняя граница отрицательна {endPoint}", ConsoleColor.DarkYellow);
+        int buff = startPoint;
+        startPoint = endPoint;
+        endPoint = buff;
+    }
+    for (int i = startPoint; i <= endPoint; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+printInConsoleWithColor("Введите верхнюю границу для суммы ряда", ConsoleColor.DarkMagenta);
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int sumOfRage = getSumOfRange(1, userNumber);
+Console.WriteLine($"Сумма чисел от 1 до {userNumber} = {sumOfRage}");
