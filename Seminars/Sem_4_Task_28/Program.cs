b12802a8611b43a02f9216e135d117fea41cafb7
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-28 02:12:56 +0300

    Product of numbers

---
+++ Seminars/Sem_4_Task_28/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N. 
+4 -> 24
+5 -> 120
+*/
+long GetMultOfRange(int startpoint, int endpoint)
+{
+    long result = 1;
+    for (int i = startpoint; i <= endpoint; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+userNumber = Math.Abs(userNumber);
+long MultOfRange = GetMultOfRange(1, userNumber);
+Console.WriteLine($"Произведение чисел от 1 до {userNumber} |по модулю| равняется {MultOfRange}!");
