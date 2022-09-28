9fc998cd5e9b3803d7ed5982d2e6c468a57d698f
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-28 16:51:31 +0300

    Sum of digits

---
+++ Seminars/Homework_Sem_4_Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int number = new Random().Next(10, 999999);
+Console.Write($"Введите произвольное число: {number}");
+int sum = 0;
+for (int i = number; i > 0; i = (i / 10))
+{
+    int remain = i % 10;
+    sum = sum + remain;
+}
+Console.WriteLine();
+
+Console.WriteLine($"Сумма цифр в числе {number} равна {sum}!");
