b2ea6be6ad8e22a64e59968608229786a370003d
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-09-28 15:38:19 +0300

    Number to power

---
+++ Seminars/Homework_Sem_4_Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Write("Введите число A: ");
+int namberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int namberB = Convert.ToInt32(Console.ReadLine());
+int resultPower = 1;
+for (int i = 0; i < namberB; i++)
+{
+    resultPower = resultPower * namberA;
+}
+
+Console.WriteLine($"Число {namberA} в степени {namberB} равно {resultPower}!");
