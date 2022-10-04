50d9bb375f1591e404dbf982cc333cc1fdec2e7c
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-04 22:04:55 +0300

    Binary conversion

---
+++ Seminars/Sem_6_Task_42/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+// int value = 45;
+// string binary = Convert.ToString(value, 2);
+
+// Console.WriteLine(binary);
+
+Console.Write("Введите десятичное число: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+int remainder;
+string result = string.Empty;
+while (decimalNumber > 0)
+{
+    remainder = decimalNumber % 2;
+    decimalNumber /= 2;
+    result = remainder.ToString() + result;
+}
+Console.WriteLine("Число в бинарном виде:  {0}", result);
