238bd597dd6d91691082307c80967596e809a9c0
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-06 19:32:27 +0300

    Point intersection lines Ошибка в округлении

--- Seminars/Homework_Sem_6_Task_43/Program.cs
+++ Seminars/Homework_Sem_6_Task_43/Program.cs
@@ -4,27 +4,17 @@
 значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
-int getCoordinateFromUser(string userInformation)
-{
-    int result = 0;
-    Console.Write($"{userInformation} ");
-    while (!int.TryParse(Console.ReadLine(), out result))
-    {
-        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation}: ");
-    }
-    return result;
-}
 Console.WriteLine("Заданы две прямые, описываемые уравнениями: y = k1 * x + b1, y = k2 * x + b2");
-Console.WriteLine("Ведите постоянные аргументы уравнений: ");
-int argumentb1 = getCoordinateFromUser("b1 = ");
-int argumentk1 = getCoordinateFromUser("k1 = ");
-int argumentb2 = getCoordinateFromUser("b2 = ");
-int argumentk2 = getCoordinateFromUser("k2 = ");
+Console.Write("Ведите значение постоянного аргумента уравнений b1 = ");
+double argumentb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите значение постоянного аргумента уравнений k1 = ");
+double argumentk1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите значение постоянного аргумента уравнений b2 = ");
+double argumentb2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите значение постоянного аргумента уравнений k2 = ");
+double argumentk2 = Convert.ToInt32(Console.ReadLine());
 
-double findCoordinatesPointIntersectionLines(int argumentb1, int argumentk1, int argumentb2, int argumentk2)
-{
-    double variableX = Math.Sign(argumentb2 - argumentb1) / (argumentk1 - argumentk2);
-    double variableY = Math.Sign(argumentk1 * variableX) + argumentb1;
-    return Console.WriteLine($"{variableX:F1}, {variableY:F1}");
-}
-Console.WriteLine($"Координаты пересечения прямых равны: ({findCoordinatesPointIntersectionLines(argumentb1, argumentk1, argumentb2, argumentk2)})");
+double variableX = Math.Sign(Math.Round((argumentb2 - argumentb1) / (argumentk1 - argumentk2), 1));
+double variableY = Math.Sign(Math.Round((argumentk1 * variableX) + argumentb1, 1));
+Console.WriteLine($"Координаты пересечения прямых равны: ({variableX:F1}; {variableY:F1})");
+
