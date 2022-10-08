c3bd7da5eb783ca662c8f4b7b7f2e7129e89c9e4
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-09 00:55:01 +0300

    Real Array Ошибка

---
+++ Seminars/Homework_Sem_7_Task_47/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+double[,] generateRealArray(int height, int width, double randomStart, double randomEnd)
+{
+    double[,] RealArray = new double[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //RealArray[i, j] = new Random().NextDouble() * 10;
+            RealArray[i, j] = new Random().NextDouble(randomStart * 10, (randomEnd + 1) * 10);
+        }
+    }
+    return RealArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void printRealArray(double[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($"{arrayToPrint[i, j]:F1}" + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] generatedArray = generateRealArray(3, 4, -10, 10);
+printRealArray(generatedArray);
