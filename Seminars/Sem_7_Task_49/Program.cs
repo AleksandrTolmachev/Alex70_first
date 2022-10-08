0ee7a91fdd32acc909ac7b55f7b64ce0020b3d6b
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-08 03:15:07 +0300

    Even array elements replaced by squares

---
+++ Seminars/Sem_7_Task_49/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+и замените эти элементы на их квадраты. 
+Например, изначально массив  выглядел вот так: 
+1 4 7 2 
+5 9 2 3 
+8 4 2 4
+*/
+
+int[,] generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] changeArray(int[,] incomingArray)
+{
+    for (int i = 0; i < incomingArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < incomingArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                incomingArray[i, j] *= incomingArray[i, j];
+            }
+        }
+    }
+    return incomingArray;
+}
+int[,] generateArray = generate2DArray(3, 4, 1, 10);
+print2DArray(generateArray);
+int[,] changedArray = changeArray(generateArray);
+print2DArray(changedArray);
