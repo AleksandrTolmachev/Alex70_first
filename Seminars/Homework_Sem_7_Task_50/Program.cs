5cdb9126c8e09800c8f8ce88368640a11ccbc75b
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-09 20:32:48 +0300

    Ошибка

---
+++ Seminars/Homework_Sem_7_Task_50/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позицию элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+5 -> 9
+индексы:
+[0][1][2][3]
+1 4 7 2
+[4][5][6][7]
+5 9 2 3
+[8][9][10][11]
+8 4 2 4
+*/
+int[,] generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = generate2DArray(3, 4, 1, 10);
+print2DArray(generatedArray);
+
+int[] generatedArrayZ = new int[12];
+int z = 0;
+for (int i = 0; i < generatedArray.GetLength(0); i++)
+{
+    for (int j = 0; j < generatedArray.GetLength(1); j++)
+    {
+        generatedArrayZ[z] = generatedArray[i, j];
+        z++;
+    }
+}
+Console.Write($"Новый одномерный массив ({generatedArrayZ[z]}, )");
+
+// Console.Write("Введите номер позиции элемента массива, который необходимо показать: ");
+// int element = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"На позиции {element} находится {generatedArrayZ.GetValue(element)}! ");
