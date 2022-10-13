bd8836eaefaf977ffe440114cb8753593c272ef6
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-13 14:46:59 +0300

    Row ordered array

---
+++ Seminars/Homework_Sem_8_Task_54/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+int[,] generate2DArray(int height,int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height,width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(randomStart, randomEnd+1 );
+        }
+    }
+    return twoDArray;
+}
+
+int[,] orderedRowArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int z = 0; z < array.GetLength(1) - 1; z++)
+      {
+        if (array[i, z] < array[i, z + 1])
+        {
+          int buffer = array[i, z + 1];
+          array[i, z + 1] = array[i, z];
+          array[i, z] = buffer;
+        }
+      }
+    }
+  }
+  return array;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Заданный массив:");
+int[,] generatedArray = generate2DArray(3,4,1,9);
+print2DArray(generatedArray);
+Console.WriteLine("Упорядоченный массив:");
+int[,] orderedArray = orderedRowArray(generatedArray);
+print2DArray(orderedArray);
