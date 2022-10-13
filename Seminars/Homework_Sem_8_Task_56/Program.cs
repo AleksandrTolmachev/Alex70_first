cc3f05491a311b9d41a025b932f096fc8678e258
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-13 16:09:24 +0300

    Find row smallest sum elements

---
+++ Seminars/Homework_Sem_8_Task_56/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка
+*/
+int[,] generate2DArray(int height,int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height,width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(randomStart, randomEnd+1 );
+        }
+    }
+    return twoDArray;
+}
+
+int SumRowElements(int[,] array, int i)
+{
+  int sumRow = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumRow += array[i,j];
+  }
+  return sumRow;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Заданный массив:");
+int[,] generatedArray = generate2DArray(4,4,1,9);
+print2DArray(generatedArray);
+
+int minSumRowArray = 0;
+int sumElementsRow = SumRowElements(generatedArray, 0);
+for (int i = 1; i < generatedArray.GetLength(0); i++)
+{
+  int bufferSumElementsRow = SumRowElements(generatedArray, i);
+  if (sumElementsRow > bufferSumElementsRow)
+  {
+    sumElementsRow = bufferSumElementsRow;
+    minSumRowArray = i;
+  }
+}
+Console.WriteLine($"Строка с наименьшей суммой: строка №{minSumRowArray}!");
