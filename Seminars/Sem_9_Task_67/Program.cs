d928bf2109407d117c132fc6c54b9c7ba67c3331
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-14 21:37:18 +0300

    Sum of digits given number

---
+++ Seminars/Sem_9_Task_67/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+int printRange(int N)
+{
+    if (N == 0)
+    {
+        return 0;
+    }
+    return N % 10 + printRange(N / 10);
+}
+
+Console.Write("Введите число N: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int summ = printRange(userNumber);
+Console.Write($"Сумма всех цифр заданного числа равна: {summ}!");
