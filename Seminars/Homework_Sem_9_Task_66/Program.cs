211a032f5396812145359187eb52518a6dee88b9
Aleksandr Tolmachev <atolmachev70@mail.ru>
2022-10-15 01:05:27 +0300

    Sum numbers from M to N

---
+++ Seminars/Homework_Sem_9_Task_66/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+Console.Write("Введите число M: ");
+int userNumberStart = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int userNumberEnd = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+SumNumbers(userNumberStart, userNumberEnd, summ);
+void SumNumbers(int start, int end, int summ)
+{
+    summ = summ + end;
+    if (start >= end)
+    {
+        Console.Write($"Сумма элементов от {start} до {userNumberEnd} равна {summ}!");
+        return;
+    }
+    SumNumbers(start, end - 1, summ);
+}
